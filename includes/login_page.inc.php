acfbef41937ab203618280627ff7c8b68f7e907c
kbastic <30803397+kbastic@users.noreply.github.com>
2017-09-11 10:46:26 -0400

    Add files via upload

---
+++ includes/login_page.inc.php
@@ -0,0 +1,32 @@
+<?php # Script 12.1 - login_page.inc.php
+// This page prints any errors associated with logging in
+// and it creates the entire login page, including the form.
+
+// Include the header:
+$page_title = 'Login';
+include ('includes/header.html');
+
+// Print any error messages, if they exist:
+if (isset($errors) && !empty($errors)) {
+	echo '<h1>Error!</h1>
+	<p class="error">The following error(s) occurred:<br />';
+	foreach ($errors as $msg) {
+		echo " - $msg<br />\n";
+	}
+	echo '</p><p>Please try again.</p>';
+}
+
+// Display the form:
+?>
+<h2 align="center">Login</h2>
+<fieldset><legend style="font-size:small">Enter your information in the form below:</legend>
+<form action="login.php" method="post">
+    </br>
+	<p>Email Address: <input type="text" name="email" size="20" maxlength="60" /> </p>
+	<p>Password: <input type="password" name="pass" size="20" maxlength="20" /></p>
+</fieldset>
+	<p align="center"><input type="submit" name="submit" value="Login" /></p>
+</form>
+
+
+<?php include ('includes/footer.html'); ?>
